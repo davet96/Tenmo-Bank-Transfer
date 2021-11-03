6ae4a2270f349ded91e59dbfae7da317983add53
David Thornburg <ibdangling@gmail.com>
2021-11-03 18:23:10 -0400

    Transfers classes

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/BalanceService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/BalanceService.java
@@ -46,43 +46,6 @@ public class BalanceService {
 }
 
 
-//    private String baseUrl;
-//    private RestTemplate restTemplate = new RestTemplate();
-//    private AuthenticatedUser currentUser;
-//    private String authToken = null;
-//    public void setAuthToken(String authToken){this.authToken = authToken;}
-    // create
-
-//    public BalanceService (String url,AuthenticatedUser currentUser) {this.baseUrl = url; this.currentUser = currentUser;}
-//    public BigDecimal balance(){BigDecimal balance = new BigDecimal(0);
-
-//    private HttpEntity makeAuthEntity() {
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setBearerAuth(currentUser.getToken());
-//            HttpEntity entity = new HttpEntity<>(headers);
-//            return entity;
-//        }
-//    try { balance = restTemplate.exchange(baseUrl + "balance/" + currentUser.getUser().getId(), HttpMethod.GET, makeAuthEntity(), BigDecimal.class);
-//      balance = balance.getBody();
-//
-//    }catch (ResourceAccessException e){
-//        e.getMessage();
- //   }
-
-//        public balance getBalance(BigDecimal usId) {
-//            Balance balance = 0;
-//            try {
-//                ResponseEntity<Reservation> response =
-//                        restTemplate.exchange(API_BASE_URL + "reservations/" + reservationId,
-//                                HttpMethod.GET, makeAuthEntity(), Reservation.class);
-//                reservation = response.getBody();
-//            } catch (RestClientResponseException | ResourceAccessException e) {
-//                BasicLogger.log(e.getMessage());
-//            }
-//            return reservation;
-//        }
-
-
 
 
 
---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransfersService.java
@@ -0,0 +1,30 @@
+package com.techelevator.tenmo.services;
+
+import com.techelevator.tenmo.model.AuthenticatedUser;
+import com.techelevator.tenmo.model.Transfers;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+public class TransfersService {
+
+    private String baseUrl;
+    private RestTemplate restTemplate = new RestTemplate();
+    private AuthenticatedUser currentUser;
+}
+
+    private HttpEntity makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(currentUser.getToken());
+        HttpEntity entity = new HttpEntity<>(headers);
+        return entity;
+    }
+
+    public Transfers getTransfer() {
+
+
+    }
+
+
+
+}
